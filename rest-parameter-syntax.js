cfcd3553751ae9fce2caf5e93a0d16ebb6de33c1
Edwin <147519340+Skopezz@users.noreply.github.com>
2024-01-05 13:39:24 +0000

    Add regular function call

--- rest-parameter-syntax.js
+++ rest-parameter-syntax.js
@@ -4,7 +4,11 @@
  */
 
 // Regular function call 
+const sumAll = (a, b, c) => a + b + c;
+let sum = sumAll(1, 2, 3);
+console.log("Sum:", sum);
 
 // Extra arguments are ignored
+
 
 // Function using ...rest
